0dc95ea3cd823f55ac4376d8864fc9193407f881
harley <gostdud@gmail.com>
2024-08-12 21:32:34 +0100

    whoag

--- fish/config.fish
+++ fish/config.fish
@@ -6,7 +6,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 alias ce="clear && exec bash"
-alias nvchad="NVIM_APPNAME=nvchad nvim"
 alias gc="git commit -a"
 alias cave="/mnt/c/winscap.exe 2 48000 16 > /tmp/cava.fifo & cava"
 alias ll="ls -la --color=auto"
