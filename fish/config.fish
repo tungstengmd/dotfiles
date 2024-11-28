08cb29c524e607e64816d5c5243463d05d20e415
Harlow :3 <gostdud@gmail.com>
2024-11-28 20:58:54 +0000

    meow!

--- fish/config.fish
+++ fish/config.fish
@@ -13,7 +13,7 @@ alias gco="git checkout"
 #---General aliases---#
 alias ls="eza"
 alias man="batman $argv"
-alias vifish="vi ~/.config/fish/config,fish"
+alias vifish="vi ~/.config/fish/config.fish"
 alias yeet="yay -Rcns"
 alias ce="clear && exec fish"
 alias cave="/mnt/c/winscap.exe 2 48000 16 > /tmp/cava.fifo & cava"
