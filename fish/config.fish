9f0a8cc0ab860c536255a6f7d649f2e35c13a928
harley 3 <gostdud@gmail.com>
2024-12-06 18:17:32 +0000

    meow

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,8 @@ starship init fish | source
 zoxide init fish | source
 atuin init fish | source
 thefuck --alias | source
+wal -R > meow.txt
+rm meow.txt
 #---Git aliases---#
 alias gc="git commit -a"
 alias git="hub"
@@ -11,13 +13,13 @@ alias gf="git pull"
 alias gp="git push"
 alias gco="git checkout"
 #---General aliases---#
-alias ls="eza"
+alias ls="eza -a"
 alias man="batman $argv"
 alias vifish="vi ~/.config/fish/config.fish"
 alias vifoot="vi ~/.config/foot/foot.ini"
 alias yeet="yay -Rcns"
 alias ce="clear && exec fish"
-alias cave="/mnt/c/winscap.exe 2 48000 16 > /tmp/cava.fifo & cava"
+# alias cave="/mnt/c/winscap.exe 2 48000 16 > /tmp/cava.fifo & cava" #
 alias cd="z"
 alias nerdfetch="nerdfetch -c" # comment out if you don't use cozette
 alias cbl="cbonsai -l"
