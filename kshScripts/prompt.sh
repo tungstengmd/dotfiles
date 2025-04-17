f968f4dd5a8a46de5ab2129788375a139b4f7960
Harlow Foxworthy <harley@the-machine.lan>
2025-04-17 16:25:51 +0100

    breaks

--- kshScripts/prompt.sh
+++ kshScripts/prompt.sh
@@ -13,5 +13,5 @@ case $gitstat in
     *"Untracked"*) symb+="U" ;&
     *"detached"*) brnch="$(git branch | head -1 | sed "s/)//")"; brnch="(${brnch##* })"
 esac
-printf "$(tput sc; tput cuf $(($(tput cols)-${#t})); echo $t; tput rc)\033[92m╭─{owo}─{"$(date +%H):$(date +%M)"}$([ $USER = root ] && echo "\033[91m" || echo "\033[93m") ${USER} \033[92min \033[30m\033[102m$(pwd | sed -e "s|^$HOME|~|" -e "s|\(\.\{0,1\}[^/]\)[^/]*/|\1/|g")\033[49m\033[92m $brnch$([ "$symb" = "" ] || echo " [$symb] ")\n${exitcolor}\% \033[0m"
+printf "\033[92m╭─{owo}─{"$(date +%H):$(date +%M)"}$([ $USER = root ] && echo "\033[91m" || echo "\033[93m") ${USER} \033[92min \033[30m\033[102m$(pwd | sed -e "s|^$HOME|~|" -e "s|\(\.\{0,1\}[^/]\)[^/]*/|\1/|g")\033[49m\033[92m $brnch$([ "$symb" = "" ] || echo " [$symb] ")\n${exitcolor}\% \033[0m"
 )'
