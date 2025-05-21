dba6f900ceb65a353a175b236f8a1b64a4a4d267
Harlow Foxworthy <harley@the-machine.lan>
2025-05-21 18:45:00 +0100

    mrrp

--- kshScripts/prompt.sh
+++ kshScripts/prompt.sh
@@ -4,7 +4,7 @@ PS1='$(e=$?
 gitstat="$(git status 2>&1)"
 symb=""
 brnch="$(echo "$(git branch --show-current 2>/dev/null)")"
-[ "$brnch" = "" ] || brnch="$(echo "($brnch)")"
+[ "$brnch" = "" ] || brnch="$(echo " ($brnch)")"
 case $gitstat in
     *"has diverged"*) symb+="%" ;&
     *"branch is behind"*) symb+="<" ;&
@@ -14,7 +14,6 @@ case $gitstat in
     *"renamed"*) symb+="R" ;;&
     *"Untracked"*) symb+="U" ;;&
     *"modified"*) symb+="M" ;&
-    *"detached"*) brnch="$(git branch | head -1 | sed "s/)//")"
+    *"detached"*) brnch="$(git branch | head -1 | sed "s/)//")"; brnch=" (${brnch##* })"
 esac
-brnch=" (${brnch##* })"
 printf "\033[92m╭─{owo}─{"$(date +%H:%M)"}$([ $USER = root ] && echo "\033[91m" || echo "\033[93m") ${USER} \033[92min \033[30m\033[102m$(pwd | sed -e "s|^$HOME|~|" -e "s|\(\.\{0,1\}[^/]\)[^/]*/|\1/|g")\033[49m\033[92m$brnch $([ "$symb" = "" ] || echo "[$symb] ")$t $e\n${exc}%% \033[0m")'
