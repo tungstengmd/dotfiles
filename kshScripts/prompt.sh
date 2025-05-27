6112bc0ffbde366bd2fa16db4b096705e335c6d8
Harlow Foxworthy <harley@the-machine.lan>
2025-05-27 12:47:27 +0100

    lessened amount of needed variables by 1

--- kshScripts/prompt.sh
+++ kshScripts/prompt.sh
@@ -1,5 +1,4 @@
-PS1='$(e=$?
-[ $e = 0 ] && { exc="\033[92m╰──"; e=""; } || { exc="\033[91mx  "; e="«\033[91m"$e"/SIG$(kill -l "$e")\033[92m»\033[0m"; }
+PS1='$([ $? = 0 ] && e="\n\033[92m╰──" || e="«\033[91m"$?"/SIG$(kill -l "$?")\033[92m»\033[0m\n\033[91mx   " 
 [[ $t != "" ]] && t="took $(($(date +%s) - ${t}))s"
 gitstat="$(git status 2>&1)"
 symb=""
@@ -16,4 +15,4 @@ case $gitstat in
     *"modified"*) symb+="M" ;&
     *"detached"*) brnch="$(git branch | head -1 | sed "s/)//")"; brnch=" (${brnch##* })"
 esac
-printf "\033[92m╭─{owo}─{"$(date +%H:%M)"}$([ $USER = root ] && echo "\033[91m" || echo "\033[93m") ${USER} \033[92min \033[30m\033[102m$(pwd | sed -e "s|^$HOME|~|" -e "s|\(\.\{0,1\}[^/]\)[^/]*/|\1/|g")\033[49m\033[92m$brnch $([ "$symb" = "" ] || echo "[$symb] ")$t $e\n${exc}%% \033[0m")'
+printf "\033[92m╭─{owo}─{"$(date +%H:%M)"}$([ $USER = root ] && echo "\033[91m" || echo "\033[93m") ${USER} \033[92min \033[30m\033[102m$(pwd | sed -e "s|^$HOME|~|" -e "s|\(\.\{0,1\}[^/]\)[^/]*/|\1/|g")\033[49m\033[92m$brnch $([ "$symb" = "" ] || echo "[$symb] ")$t$e%% \033[0m")'
