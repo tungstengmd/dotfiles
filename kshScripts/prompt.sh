6415813bca74ea991ca5e228ff9908dfb9b59516
Aster Foxworthy <gostdud@gmail.com>
2025-07-10 18:06:01 +0100

    yay

--- kshScripts/prompt.sh
+++ kshScripts/prompt.sh
@@ -1,6 +1,10 @@
+function realtime.get {
+    [[ $failsafe = 0 ]] && .sh.value="$(($(date +%s) - $t))" || realtime=0
+}
 function RPROMPT.get {
-    [[ -n ${SSH_CLIENT} ]] && { .sh.value="$(who am i | tr -d '()' | awk '{print $5}')"; [[ $t = "" ]] || .sh.value+=", "; }
-    .sh.value+="took $(($(date +%s) - $t))s"
+    [[ -n ${SSH_CLIENT} ]] && { .sh.value="$(who m i | tr -d '()' | awk '{print $5}')"; [[ $t = "" ]] || .sh.value+=", "; }
+    .sh.value+="took ${realtime}s"
+    failsafe=1
 }
 PS1='$([ $? = 0 ] && e="\n\E[92m╰──" || e="«\E[91m"$?"/SIG`kill -l "$?"`\E[92m»\E[0;91m\nx  " 
 brnch="$(git branch --show-current 2>/dev/null)"
@@ -15,6 +19,6 @@ case `git status 2>&1` in
     *"Untracked"*) symb+="U" ;;&
     *"modified"*) symb+="M" ;;&
     *"detached"*) brnch="$(git branch | head -1 | sed "s/)//")"; brnch=" (${brnch##* })" ;;
-    ?) symb=" "
+    ?) symb=
 esac
 print -n "\E[0;92m╭─{owo}─{`date +%H:%M`}`[ $USER = root ] && echo "\E[91m" || echo "\E[93m"` ${USER} \E[92min \E[7m`pwd | sed -e "s|^$HOME|~|" -e "s|\(\.\{0,1\}[^/]\)[^/]*/|\1/|g"`\E[27m$brnch`[ "$symb" = "" ] && echo " " || echo " [$symb] "`$e% \E[0m")'
