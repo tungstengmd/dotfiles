c7acbf007b49f1736aaf82023c14ab0e2bb07123
Aster Foxworthy <gostdud@gmail.com>
2025-07-19 22:19:17 +0100

    silly

--- kshScripts/prompt.sh
+++ kshScripts/prompt.sh
@@ -7,7 +7,7 @@ function RPROMPT.get {
     failsafe=1
 }
 PS1='$(e=$?
-[ $e = 0 ] && e="\n\E[92m╰──" || e=" «\E[91m"$e"/SIG`kill -l "$e"`\E[92m»\E[0;91m\nx  " 
+[ $e = 0 ] && { owo="\E[92mowo"; e="\n\E[92m╰──"; } || { owo="\E[91momo\E[92m"; e=" «\E[91m"$e"/SIG`kill -l "$e"`\E[92m»\E[0;91m\nx  "; } 
 brnch="$(git branch --show-current 2>/dev/null)"
 [ "$brnch" = "" ] || brnch="$(echo " ($brnch)")"
 case `git status 2>&1` in
@@ -22,4 +22,4 @@ case `git status 2>&1` in
     *"detached"*) brnch="$(git branch | head -1 | sed "s/)//")"; brnch=" (${brnch##* })" ;;
     ?) symb=
 esac
-print -n "\E[0;92m╭─{owo}─{`date +%H:%M`}`[ $USER = root ] && echo "\E[91m" || echo "\E[93m"` ${USER} \E[92min \E[7m`pwd | sed -e "s|^$HOME|~|" -e "s|\(\.\{0,1\}[^/]\)[^/]*/|\1/|g"`\E[27m$brnch`[ "$symb" = "" ] || echo " [$symb]"`$e% \E[0m")'
+print -n "\E[0;92m╭─{${owo}}─{`date +%H:%M`}`[ $USER = root ] && echo "\E[91m" || echo "\E[93m"` ${USER} \E[92min \E[7m`pwd | sed -e "s|^$HOME|~|" -e "s|\(\.\{0,1\}[^/]\)[^/]*/|\1/|g"`\E[27m$brnch`[ "$symb" = "" ] || echo " [$symb]"`$e% \E[0m")'
