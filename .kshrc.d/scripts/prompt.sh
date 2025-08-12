960733abbf04f23d7097868af7ce5500ba29a2b7
Aster Foxworthy <gostdud@gmail.com>
2025-08-12 17:53:29 +0100

    fixes and an addition

--- .kshrc.d/scripts/prompt.sh
+++ .kshrc.d/scripts/prompt.sh
@@ -3,17 +3,17 @@ function PWD_TRUNC.get {
 }
 function RPROMPT.get {
     #---check version for common tools or languages---#
-    [[ -e `pwd`/package.json ]] && type node >/dev/null && .sh.value+=" `node -v`, "
-    [[ -e `pwd`/pyproject.toml ]] && type python >/dev/null && .sh.value+=" v`python -V | awk '{print $2}'`, "
-    [[ -e `pwd`/Cargo.toml ]] && type rustc >/dev/null && .sh.value+=" v`rustc -V | awk '{print $2}'`, "
-    [[ -e `pwd`/Gemfile* ]] && type ruby >/dev/null && .sh.value+=" v `ruby -v | awk '{print $2}'`, "
-    [[ -e `pwd`/Package.swift ]] && type swift >/dev/null && .sh.value+=" v`swift -version | awk '{print $4}'`, "
-    [[ -e `pwd`/go.mod ]] && type go >/dev/null && .sh.value+=" `go version | awk '{gsub("go", "v", $3); print $3}'`, "
-    [[ -e `pwd`/artisan ]] && type php >/dev/null && .sh.value+=" v`php -v | awk '{print $2}'`, "
-    [[ -e `pwd`/Dockerfile ]] && { type docker >/dev/null && .sh.value+=" v`docker -v | awk '{print $3}'`, " || type podman >/dev/null && .sh.value+=" v`podman -v | awk '{print $3}'`, "; }
-    [[ -e `pwd`/.cljfmt.edn ]] && type clj >/dev/null && .sh.value+=" v`clj --version | awk '{print $4}'`, "
-    [[ -e `pwd`/tsconfig.json ]] && type tsc >/dev/null && .sh.value+=" v`tsc -v | awk '{print $4}'`, "
-    [[ -e `pwd`/build.zig ]] && type zig >/dev/null && .sh.value+=" `zig version`, "
+    [[ -e `pwd`/package.json ]] && type node 2>&1 >/dev/null && .sh.value+=" `node -v`, "
+    [[ -e `pwd`/pyproject.toml ]] && type python 2>&1 >/dev/null && .sh.value+=" v`python -V | awk '{print $2}'`, "
+    [[ -e `pwd`/Cargo.toml ]] && type rustc 2>&1 >/dev/null && .sh.value+=" v`rustc -V | awk '{print $2}'`, "
+    [[ -e `pwd`/Gemfile* ]] && type ruby 2>&1 >/dev/null && .sh.value+=" v `ruby -v | awk '{print $2}'`, "
+    [[ -e `pwd`/Package.swift ]] && type swift 2>&1 >/dev/null && .sh.value+=" v`swift -version | awk '{print $4}'`, "
+    [[ -e `pwd`/go.mod ]] && type go 2>&1 >/dev/null && .sh.value+=" `go version | awk '{gsub("go", "v", $3); print $3}'`, "
+    [[ -e `pwd`/artisan ]] && type php 2>&1 >/dev/null && .sh.value+=" v`php -v | awk '{print $2}'`, "
+    [[ -e `pwd`/Dockerfile ]] && { type docker 2&>1 >/dev/null && .sh.value+=" v`docker -v | awk '{print $3}'`, " || type podman 2>&1 >/dev/null && .sh.value+=" v`podman -v | awk '{print $3}'`, "; }
+    [[ -e `pwd`/.cljfmt.edn ]] && type clj 2>&1 >/dev/null && .sh.value+=" v`clj --version | awk '{print $4}'`, "
+    [[ -e `pwd`/tsconfig.json ]] && type tsc 2&>1 >/dev/null && .sh.value+=" v`tsc -v | awk '{print $4}'`, "
+    [[ -e `pwd`/build.zig ]] && type zig 2>&1 >/dev/null && .sh.value+=" `zig version`, "
     #---now for the other stuff---#
     [[ -n ${SSH_CLIENT} ]] && { .sh.value+="$(who -m | tr -d '()' | awk '{print $5" ("$2")"}')"; [[ $t = "" ]] || .sh.value+=", "; } || .sh.value+="`tty | sed -e 's|/dev/||'`, "
     [[ -n $VIRTUAL_ENV || -n $PIPENV_ACTIVE || -n $CONDA_DEFAULT_ENV ]] && .sh.value+="venv active, "
@@ -36,7 +36,7 @@ function PS2.get {
 trap '[[ ${istrans:-u} = u  ]] || t="$(date +%s)"; [[ $istrans = 0 && $TERM != dumb ]] && { tput cuu $((`fc -lnN0 | sed "s/[[:blank:]]//" | wc -l`+1)); tput ed; print -n "\E[92m\E[7m$PWD_TRUNC\E[27m-%\E[0m "; fc -lnN0 | sed "s/[[:blank:]]//"; istrans=; }; failsafe=0' DEBUG
 PS1=$'$(e=$?
 [ $e = 0 ] && { owo="\E[92mowo"; e="\n\E[92m╰──"; } || { owo="\E[91momo\E[92m"; e=" «\E[91m"$e"/SIG`kill -l "$e"`\E[92m»\E[0;91m\nx  "; } 
-brnch="`git branch --show-current 2>/dev/null`"
+brnch="`git branch --show-current 2>&1 >/dev/null`"
 [ "$brnch" = "" ] || brnch="`echo " ($brnch)"`"
 case `git status 2>&1` in
     *"has diverged"*) symb+="%" ;&
