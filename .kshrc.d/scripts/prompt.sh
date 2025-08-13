b30dc1553d4645f907da0c39dc0991e4a9a15797
Aster Foxworthy <gostdud@gmail.com>
2025-08-13 15:08:04 +0100

    wawa

--- .kshrc.d/scripts/prompt.sh
+++ .kshrc.d/scripts/prompt.sh
@@ -1,5 +1,5 @@
 function PWD_TRUNC.get {
-    .sh.value="`pwd | sed -e "s|^$HOME|~|" -e "s|\(\.\{0,1\}[^/]\)[^/]*/|\1/|g"`"
+    .sh.value="`pwd | sed -e "s|$HOME|~|" -e "s|\(\.\{0,1\}[^/]\)[^/]*/|\1/|g"`"
 }
 function RPROMPT.get {
     #---check version for common tools or languages---#
@@ -36,7 +36,7 @@ function PS2.get {
 trap '[[ ${istrans:-u} = u  ]] || t="$(date -e)"; [[ $istrans = 0 && $TERM != dumb ]] && { tput cuu $((`fc -lnN0 | sed "s/[[:blank:]]//" | wc -l`+1)); tput ed; print -n "\E[92m\E[7m$PWD_TRUNC\E[27m-%\E[0m "; fc -lnN0 | sed "s/[[:blank:]]//"; istrans=; }; failsafe=0' DEBUG
 PS1=$'$(e=$?
 [ $e = 0 ] && { owo="\E[92mowo"; e="\n\E[92m╰──"; } || { owo="\E[91momo\E[92m"; e=" «\E[91m"$e"/SIG`kill -l "$e"`\E[92m»\E[0;91m\nx  "; } 
-brnch="`git branch --show-current 2>&1 >/dev/null`"
+brnch="`git branch --show-current 2>/dev/null`"
 [ "$brnch" = "" ] || brnch="`echo " ($brnch)"`"
 case `git status 2>&1` in
     *"has diverged"*) symb+="%" ;&
