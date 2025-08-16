43651068bbd7e34b7e12303059d6524096a9c092
Aster Foxworthy <gostdud@gmail.com>
2025-08-16 13:57:40 +0100

    move this bc of it getting in the way of lang detection

--- .kshrc.d/scripts/prompt.sh
+++ .kshrc.d/scripts/prompt.sh
@@ -4,6 +4,7 @@ function PWD_TRUNC.get {
 }
 function RPROMPT.get {
     #---check version for common tools or languages---#
+    [[ -e `pwd`/Dockerfile ]] || [[ -e `pwd`/.dockerignore ]] && { type docker 2&>1 >/dev/null && .sh.value+=" v`docker -v | awk '{print $3}'`, " || type podman 2>&1 >/dev/null && .sh.value+=" v`podman -v | awk '{print $3}'`, "; }
     [[ -e `pwd`/package.json ]] && { `# go through all the runtimes`; { type node 2>&1 >/dev/null && .sh.value+=" `node -v`, "; } || { type deno 2>&1 >/dev/null && .sh.value+=" `deno -v | sed 's/deno /v/'`, "; } || { type bun 2>&1 >/dev/null && .sh.value+=" v`bun -v`, "; }; }
     [[ -e `pwd`/pyproject.toml ]] && type python 2>&1 >/dev/null && .sh.value+=" v`python -V | awk '{print $2}'`, "
     [[ -e `pwd`/Cargo.toml ]] && type rustc 2>&1 >/dev/null && .sh.value+=" v`rustc -V | awk '{print $2}'`, "
@@ -11,7 +12,6 @@ function RPROMPT.get {
     [[ -e `pwd`/Package.swift ]] && type swift 2>&1 >/dev/null && .sh.value+=" v`swift -version | awk '{print $4}'`, "
     [[ -e `pwd`/go.mod ]] && type go 2>&1 >/dev/null && .sh.value+=" `go version | awk '{gsub("go", "v", $3); print $3}'`, "
     [[ -e `pwd`/artisan ]] && type php 2>&1 >/dev/null && .sh.value+=" v`php -v | awk '{print $2}'`, "
-    [[ -e `pwd`/Dockerfile ]] || [[ -e `pwd`/.dockerignore ]] && { type docker 2&>1 >/dev/null && .sh.value+=" v`docker -v | awk '{print $3}'`, " || type podman 2>&1 >/dev/null && .sh.value+=" v`podman -v | awk '{print $3}'`, "; }
     [[ -e `pwd`/.cljfmt.edn ]] && type clj 2>&1 >/dev/null && .sh.value+=" v`clj --version | awk '{print $4}'`, "
     [[ -e `pwd`/tsconfig.json ]] && type tsc 2&>1 >/dev/null && .sh.value+=" v`tsc -v | awk '{print $4}'`, "
     [[ -e `pwd`/build.zig ]] && type zig 2>&1 >/dev/null && .sh.value+=" `zig version`, "
